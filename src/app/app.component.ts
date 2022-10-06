a7f30ba237e98c039e6fe017aaf114a92f583ad7
Rhon Umahon <65482394+rhonumahon@users.noreply.github.com>
2022-10-07 00:28:19 +0800

    server configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //SOURCE - https://www.javaguides.net/2020/11/how-to-deploy-angular-application-to-heroku.html, https://www.youtube.com/watch?v=HWBSSC7Vbg0
   title = 'fish-shop';
 }
